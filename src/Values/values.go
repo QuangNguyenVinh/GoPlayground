83e819825f2800aed0a17f4f18065d7db4d0c615
Quang Nguyen <vinhquangnguyen2@gmail.com>
2023-04-30 15:34:09 +0700

    Add Values example

---
+++ src/Values/values.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Go" + "Playground")
+	fmt.Println("1 + 1 = ", 1+1)
+	fmt.Println("8.0 / 3.0 = ", 8.0/3.0) // = 2.66
+	fmt.Println("8.0 / 3 = ", 8.0/3)     // = 2.66..
+	fmt.Println("8 / 3 = ", 8/3)         // = 2
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+}
